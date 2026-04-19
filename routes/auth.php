5b8b16ccaf2cda97acc9415bec3e37f515fd5ec7
daffamarch <muhammadmarchdiansyah@gmail.com>
2026-04-19 19:20:16 +0700

    fix: standardized all auth and verification routes to pass CI

--- routes/auth.php
+++ routes/auth.php
@@ -5,24 +5,25 @@ use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Route::middleware('guest')->group(function () {
-
     Volt::route('forgot-password', 'auth.forgot-password')
-        ->name('forgotPassword');
+        ->name('password.request');
+
 
     Volt::route('reset-password/{token}', 'auth.reset-password')
-        ->name('resetPassword');
+        ->name('password.reset');
 });
 
 Route::middleware('auth')->group(function () {
+
     Volt::route('verify-email', 'auth.verify-email')
-        ->name('verifyEmailNotice');
+        ->name('verification.notice');
 
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
-        ->name('verifyEmailAction');
+        ->name('verification.verify');
 
     Volt::route('confirm-password', 'auth.confirm-password')
-        ->name('confirmPassword');
+        ->name('password.confirm');
 });
 
 Route::post('logout', App\Livewire\Actions\Logout::class)
